8f88bbd1b005f3db16c734d9f4499fac7ccf5d47
Zachary Viggiani <zach@bigmojo.net>
2017-03-28 16:07:58 -0400

    Final version of excercise 2 file

---
+++ excercise2.rb
@@ -0,0 +1,15 @@
+# Tip Calculation
+
+puts "your 20% tip"
+
+puts 55 * 1.02
+
+# Adding strings to integers "whole numbers"
+# need to pt "arounud number"
+puts "55" + "Text"
+
+# String Interpolation
+puts "your total is #{122222 * 77744}"
+
+# true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
