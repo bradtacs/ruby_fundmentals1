742744909c2643cc84935621465a600012d11e07
Zachary Viggiani <zach@bigmojo.net>
2017-03-28 16:05:51 -0400

    final version of excercis one file

---
+++ excercise1.rb
@@ -0,0 +1,52 @@
+w
+#  numbers
+
+puts 1 +  2
+
+puts 9
+
+puts 00 + 0
+
+puts 9
+
+puts 0
+
+puts +9
+
+
+#  strings
+
+puts 'Hello world'
+
+puts "Betty's pie shop"
+
+
+# is a means of embedding Ruby code into a string by wrapping it in special characters like so: #{code goes here}. Try out the example below:
+
+puts "Ada Lovelace lived for #{1852 - 1815} years"
+
+# escape sequemces
+
+puts "Hello\t\world"
+
+puts "Hello\b\b\b\bGoodbye world"
+
+
+puts "1. Hello\n2. World" #  number at front
+
+ # && and
+
+puts (1 < 3) && (3 < 5)
+
+
+puts (1 > 1) && (2 > 2)
+
+# || or
+
+puts (1 == 2) || (2 < 3)
+
+
+puts (x1 != 2) || (2 < 3)
+
+
+puts (1 == 2) || (2 == 3)
