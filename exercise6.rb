f3037f6df475020e484b3e0ce82c5386069b1fa1
Zachary Viggiani <zach@bigmojo.net>
2017-03-28 17:59:17 -0400

    added in while loop and got it not kicking out of the loop until you walk

--- exercise6.rb
+++ exercise6.rb
@@ -1,17 +1,24 @@
 
-puts "Hello do you want to walk or run or go home?"
-# gets stopps for a an answer
-walkorRun = gets.chomp  # add in chomp to get rid of any white space!
-
 distance = 9
 
-if walkorRun == "walk"
-  distance + 1
-  puts "Your trip will be #{distance} KMs"
+while true  # keeps looping through until you get kicked out of the loop
+  puts "Hello do you want to walk or run or go home?"
+  # gets stopps for a an answer
+  walkorRun = gets.chomp  # add in chomp to get rid of any white space!
 
-elsif walkorRun == "run"
-   distance + 5
-   puts "Your trip will be #{distance} KMs"
-else walkorRun == "Go home"
-  puts "Have a safe trip home!"
+  if walkorRun == "walk"
+    distance + 1
+    puts "Your trip will be #{distance} KMs"
+
+  elsif walkorRun == "run"
+     distance + 5
+     puts "Your trip will be #{distance} KMs"
+
+  else walkorRun == "Go home"
+    puts "Have a safe trip home!"
+    # break out!
+    break
+
+  end
+
 end
